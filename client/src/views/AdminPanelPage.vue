7a2d5eb3c21b3fbda4599ccd241930b9b0db8325
Roman Rub <ro.rub@mail.ru>
2024-06-03 01:29:42 +0700

    Add AdminPanel and Some Changes

--- client/src/views/AdminPanelPage.vue
+++ client/src/views/AdminPanelPage.vue
@@ -3,7 +3,74 @@
 </script>
 
 <template>
-  <h1>Administrator Panel Page</h1>
+  <div className="admin_panel__main">
+    <div className="admin_panel__left_container">
+      <nav className="admin_panel__navigation">
+        <button className="navigation__title choosed">Редактирование</button>
+        <button className="navigation__title">Просмотр заказов</button>
+      </nav>
+      <div className="admin_panel__edit_container">
+        <ul className="edit_container__title_list">
+          <li className="edit_container__title_list_el">
+            Название
+          </li>
+          <li className="edit_container__title_list_el">
+            Автор
+          </li>
+          <li className="edit_container__title_list_el">
+            Жанр
+          </li>
+          <li className="edit_container__title_list_el">
+            Категория
+          </li>
+          <li className="edit_container__title_list_el">
+            Цена, руб.
+          </li>
+          <li className="edit_container__title_list_el">
+            Кол-во, штук
+          </li>
+        </ul>
+        <ul className="edit_container__product_list">
+          <li className="edit_container__product_list_el">
+            Книга про Васю Пупкина
+          </li>
+          <li className="edit_container__product_list_el">
+            Книга про Васю Пупкина
+          </li>
+          <li className="edit_container__product_list_el">
+            Книга про Васю Пупкина
+          </li>
+          <li className="edit_container__product_list_el">
+            Книга про Васю Пупкина
+          </li>
+          <li className="edit_container__product_list_el">
+            Книга про Васю Пупкина
+          </li>
+          <li className="edit_container__product_list_el">
+            Книга про Васю Пупкина
+          </li>
+        </ul>
+        <Edit/>
+        <Trash/>
+      </div>
+    </div>
+    <form>
+      <h3>Поиск товара(-ов)</h3>
+      <label>Поиск</label>
+      <input>
+      <label>Цена</label>
+      <div>
+        <input>
+        <input>
+      </div>
+      <label>Поиск в категории</label>
+      <input placeholder="Все категории">
+      <div>
+        <BurgerBtn2/>
+      </div>
+      <button>Найти</button>
+    </form>
+  </div>
 </template>
 
 <style>
